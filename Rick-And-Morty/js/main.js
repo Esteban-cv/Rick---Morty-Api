a5b74d556f0a663706329cfb8c165d9ff14a4028
Esteban C.v <stphgamer@gmail.com>
2025-09-15 08:47:59 -0500

    API FINISHED

---
+++ Rick-And-Morty/js/main.js
@@ -0,0 +1,69 @@
+function fillTable(data) {
+    let table = "";
+    const characters = data.results;
+    
+    characters.forEach(character => {
+        table += `
+        <tr>
+            <td>${character.id}</td>
+            <td>${character.name}</td>
+            <td>${character.species}</td>
+            <td>${character.status}</td>
+            <td>
+                <button class="btn btn-primary btn-sm btnMoreDetails" data-id="${character.id}">
+                    <i class="fas fa-info-circle me-1"></i>Ver Detalles
+                </button>
+            </td>
+        </tr>
+        `;
+    });
+    
+    $("#charactersTable").html(table);
+    // volver a inicializar
+    $('#tablapersonajes').DataTable();
+}
+
+function showCharacterDetails(character) {
+    $("#characterImage").attr("src", character.image);
+    $("#characterName").text(character.name);
+    $("#characterSpecies").text(character.species);
+    $("#characterStatus").text(character.status);
+    $("#characterGender").text(character.gender);
+    $("#characterOrigin").text(character.origin.name);
+    $("#characterLocation").text(character.location.name);
+    
+    $("#modalpersonaje").modal("show");
+}
+
+async function getAllCharacters() {
+    let allCharacters = [];
+    let nextPage = "https://rickandmortyapi.com/api/character/";
+
+    while (nextPage) {
+        try {
+            const response = await fetch(nextPage);
+            const data = await response.json();
+            allCharacters = allCharacters.concat(data.results);
+            nextPage = data.info.next;
+        } catch (error) {
+            console.error("Error fetching characters:", error);
+            break;
+        }
+    }
+
+    return { results: allCharacters };
+}
+
+$(document).on("click", "#show", async function() {
+    const data = await getAllCharacters();
+    fillTable(data);
+});
+
+$(document).on('click', '.btnMoreDetails', function() {
+    const id = $(this).data('id');
+    
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(response => response.json())
+        .then(character => showCharacterDetails(character))
+        .catch(error => console.error("Error en el llamado a la API: ", error));
+});
